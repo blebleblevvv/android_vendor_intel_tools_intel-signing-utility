921b55c2a53b59526fa6f54986b3926eac102a9f
Vincent Tinelli <vincent.tinelli@intel.com>
2013-12-02 12:13:53 +0100

    isu: fix isu_wrapper to discard warning messages
    BZ: 155825
    
    Change-Id: Ida2285ca38fdc37468a74e40a1c81e75473be8cf
    Signed-off-by: Vincent Tinelli <vincent.tinelli@intel.com>

--- isu_wrapper.sh
+++ isu_wrapper.sh
@@ -6,11 +6,11 @@ pub_key=${3}
 
 bootimage=`tempfile`
 manifest=`tempfile`
-tput cup 0 0 2>&1 >/dev/null
+tput cup 0 0 >/dev/null 2>&1
 cat - > ${bootimage}
 ${isu_exec} -h 0 -i ${bootimage} -o ${manifest} -l \
 ${priv_key} -k \
-${pub_key} -t 4 -p 2 -v 1 2>&1 >/dev/null
+${pub_key} -t 4 -p 2 -v 1 >/dev/null 2>&1
 cat ${manifest}_OS_manifest.bin
 rm -f ${bootimage}
 rm -f ${manifest}*
